e57ed2751e01bb72f224af5ddcb4c6e1f41aa98f
kx <kaixuan.sun@boonray.com>
2022-01-04 17:02:05 +0800

    框架初步文件定义

---
+++ try_framework/container.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   container.py
+@Time    :   2022/01/04 15:33:50
+@Author  :   sk 
+@Version :   1.0
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+# here put the import lib
+
+"""
+Define service core container
+"""
+
+class ServiceContainer(object):
+    pass
---
+++ try_framework/runners.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+'''
+@File    :   runners.py
+@Time    :   2022/01/04 15:31:21
+@Author  :   sk 
+@Version :   1.0
+@License :   (C)Copyright 2017-2018, Liugroup-NLPR-CASIA
+@Desc    :   None
+'''
+
+# here put the import lib
+
+"""
+Define services runner
+"""
+
+class ServicesRunner(object):
+
+    pass
